213801d99fd02f08c8553fa0e10aeb2217da7670
Nishant Mishra <mnishant.work@gmail.com>
2025-05-30 02:06:47 +0530

    Add files via upload

---
+++ missing_number.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int missing(vector <int> &arr){
+    int n = arr.size();
+    int sum = n*(n+1)/2;
+    int sum_arr = 0;
+    for (int i = 0; i < n; i++){
+        sum_arr += arr[i];
+    }
+    return sum - sum_arr;
+}
+int main(){
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+    int missing_number =  missing(arr);
+    cout << missing_number;
+    return 0;
+}
