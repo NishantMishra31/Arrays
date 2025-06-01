0d109b2a73ce037c4e1e67e889a90a8a7f66b940
Nishant Mishra <mnishant.work@gmail.com>
2025-06-01 22:52:55 +0530

    Add files via upload

---
+++ longest_subarray_sum_k.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longest_subarray_with_sum_k(vector <int> &arr, long long k){
+    long long n = arr.size();
+    map <long long, int> preMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++){
+        sum += arr[i];
+        if ( sum == k){
+            maxLen = max(maxLen , i+1);
+        }
+        long long rem = sum - k;
+        if ( preMap.find(rem) != preMap.end()){
+            int len = i - preMap[rem];
+            maxLen = max(maxLen, len);
+        }
+        if ( preMap.find(sum) == preMap.end()){
+            preMap[sum] = i;
+        }
+    }
+    return maxLen;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    long long k;
+    cin >> k;
+    // call
+    int result = longest_subarray_with_sum_k(arr, k);
+    cout << result;
+    return 0;
+}
