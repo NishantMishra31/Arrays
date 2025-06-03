545fc0680c7e45cf61c1f22e3947c4c599dbd944
Nishant Mishra <mnishant.work@gmail.com>
2025-06-03 16:58:11 +0530

    Add files via upload

---
+++ sort_array_of_0,1,2.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sort_array(vector <int> &arr){
+    int n = arr.size();
+    int count1 = 0, count2 = 0, count3 = 0;
+    for (int i = 0; i < n; i++){
+        if (arr[i] == 0) count1 ++;
+        else if (arr[i] == 1) count2 ++;
+        else if (arr[i] == 2) count3 ++;
+    }
+    for (int i = 0; i < count1; i++){
+        arr[i] = 0;
+    }
+    for (int i = count1; i < count1 + count2; i++){
+        arr[i] = 1;
+    }
+    for (int i = count1 + count2; i < n; i++){
+        arr[i] = 2;
+    }
+}
+
+void sort_array2(vector <int> &arr){
+    //the dutch national flag approach
+    int n = arr.size();
+    int low = 0, mid = 0, high = n - 1;
+    while(mid <= high){
+        if (arr[mid] == 0){
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1){
+            mid++;
+        }
+        else{
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    sort_array(arr);
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
