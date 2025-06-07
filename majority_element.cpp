98fb881efbfbde28a4e96bc111e3532dbc45db3c
Nishant Mishra <mnishant.work@gmail.com>
2025-06-08 00:00:10 +0530

    Add files via upload

---
+++ majority_element.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int majority_element(vector <int> &arr){
+    int n = arr.size();
+    map<int, int> mp;
+    for(int i = 0; i < n; i++){
+        mp[arr[i]]++;
+    }
+    for(auto it : mp){
+        if (it.second > n/2){
+            return it.first;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    cout << majority_element(arr);
+    return 0;
+}
