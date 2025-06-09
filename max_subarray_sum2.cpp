a661ea6274f40675d7986812f0a48dcd4a5c31a2
Nishant Mishra <mnishant.work@gmail.com>
2025-06-09 15:06:23 +0530

    Add files via upload

---
+++ max_subarray_sum2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long maxsubarraysum(vector <int> &arr){
+    int n = arr.size();
+    int maxsum = arr[0];
+    int sum = arr[0];
+    for (int i = 1; i < n; i++) {
+        sum = max(sum + arr[i], arr[i]);
+        maxsum = max(sum, maxsum);
+    }
+    return maxsum;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    //call
+    cout << maxsubarraysum(arr);
+    return 0;
+}
