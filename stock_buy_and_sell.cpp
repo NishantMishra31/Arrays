88eea5512a6ec8e7955d953740754f2a1175d669
Nishant Mishra <mnishant.work@gmail.com>
2025-06-10 16:30:13 +0530

    Add files via upload

--- stock_buy_and_sell.cpp
+++ stock_buy_and_sell.cpp
@@ -1,12 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// dynamic programming approach to solve the classic stock buy-sell problem
 int maxProfit(vector<int>& prices) {
     int n = prices.size();
     int mini =  prices[0];
     int maxprofit = 0;
-    for (int i = 0; i < n ; i++){
+    for (int i = i; i < n ; i++){
         int cost = prices[i] - mini;
         maxprofit = max(maxprofit, cost);
         mini = min(mini, prices[i]);
