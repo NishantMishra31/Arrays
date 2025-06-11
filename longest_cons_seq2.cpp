1437c84545743212d16cec21b246397e6e0ee0f2
Nishant Mishra <mnishant.work@gmail.com>
2025-06-11 20:12:46 +0530

    Update longest_cons_seq2.cpp

--- longest_cons_seq2.cpp
+++ longest_cons_seq2.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// this approach uses a hash set to store the unique elements in the array
+// TC is O(3n) if set allows lookup of O(1)
+// SC is O(n) for storing the elements
 int longest_cons_seq(vector <int> &arr){
     int n = arr.size();
     if (n == 0) return 0;
