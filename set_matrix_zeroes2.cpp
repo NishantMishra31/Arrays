78818a04738337c5839e8f061e65078b80ff247b
Nishant Mishra <mnishant.work@gmail.com>
2025-06-12 13:08:29 +0530

    Add files via upload

---
+++ set_matrix_zeroes2.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void set_matrix_zeroes(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    vector <int> rows(n, 0);
+    vector <int> cols(m, 0);
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            if (matrix[i][j] == 0){
+                rows[i] = 1;
+                cols[j] = 1;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++){
+        for ( int j = 0; j < m; j++){
+            if (rows[i] == 1 || cols[j] == 1){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    return;
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> arr(n, vector<int>(m));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> arr[i][j];
+        }
+    }
+    //call
+    set_matrix_zeroes(arr);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
