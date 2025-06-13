ce3cb505d305b4a1e5620b9f192c605798044e22
Nishant Mishra <mnishant.work@gmail.com>
2025-06-13 18:15:20 +0530

    Add files via upload

---
+++ spiral_matrix.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> spiral_matrix(vector<vector<int>> &arr){
+    int n = arr.size();
+    int m = arr[0].size();
+    int left = 0, right = m - 1;
+    int top = 0, bottom = n - 1;
+    vector <int> ans;
+    while (top <= bottom && left <= right){
+        for(int i = left; i <= right; i++){
+            ans.push_back(arr[top][i]);
+        }
+        top++;
+        for(int i = top; i <= bottom; i++){
+            ans.push_back(arr[i][right]);
+        }
+        right--;
+        if (top <= bottom){
+            for(int i = right; i >= left; i--){
+                ans.push_back(arr[bottom][i]);
+            }
+        }
+        bottom--;
+        if(left <= right){
+            for(int i = bottom; i >= top; i--){
+                ans.push_back(arr[i][left]);
+            }
+            left++;
+        }
+    }
+    return ans;
+}
+
+int main (){
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> arr(n, vector<int> (m));
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            cin >> arr[i][j];
+        }
+    }
+    // call
+    vector <int> ans = spiral_matrix(arr);
+    for( int i = 0; i < ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+    return 0;
+}
