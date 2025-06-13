8e10a816201b9eaccbb169ee7e10448a8cb9c509
Nishant Mishra <mnishant.work@gmail.com>
2025-06-13 18:14:54 +0530

    Add files via upload

---
+++ rotate_image.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate_image(vector<vector<int>> &arr){
+    int n = arr.size();
+    vector<vector<int>> ans(n, vector<int>(n));
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            ans[j][n-1-i] = arr[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            arr[i][j] = ans[i][j];
+        }
+    }
+    return;
+}
+
+int main (){
+    int n;
+    cin >> n ;
+    vector<vector<int>> arr(n, vector<int>(n));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+        }
+    }
+    // call
+    rotate_image(arr);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
